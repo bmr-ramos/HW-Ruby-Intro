8761bd0e74a5d5dff4949699f420b1429d3073d5
br344737 <br344737@gmail.com>
2025-09-09 20:39:13 -0500

    Part 1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,18 +1,33 @@
 # frozen_string_literal: true
 
 # Part 1
+# bundle exec rspec -fd spec/part1_spec.rb
 
 def sum(arr)
   # YOUR CODE HERE
+  arr.sum
 end
-
+  
 def max_2_sum(arr)
   # YOUR CODE HERE
+  arr.max(2).sum
 end
 
 def sum_to_n?(arr, number)
   # YOUR CODE HERE
+  return false if arr.nil? || arr.length < 2
+
+  seen = {}
+  arr.each do |x|
+    return true if seen[number - x]
+    seen[x] = true
+  end
+
+  false
 end
+
+
+
 
 # Part 2
 
